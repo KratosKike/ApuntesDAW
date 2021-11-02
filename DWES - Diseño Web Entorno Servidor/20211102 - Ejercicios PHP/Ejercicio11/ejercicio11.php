3d084e5b2b3bef68baed5ed46c2065d4043ac019
Enrique Lechuga <kratoskike@gmail.com>
2021-11-02 16:14:30 +0100

    ejercicio 11 hecho

---
+++ DWES - Diseño Web Entorno Servidor/20211102 - Ejercicios PHP/Ejercicio11/ejercicio11.php
@@ -0,0 +1,35 @@
+<?php
+$nombrecookie="color";
+$color = $_POST['color'];
+echo $color;
+setcookie("color",$color,0,);
+
+if (isset($_COOKIE['color'])){
+    $colorCookie = $_COOKIE['color'];
+    echo $colorCookie;
+
+    switch($colorCookie){
+        case "rojo":
+            echo '<body style="background-color:red">';
+
+        break;
+        case "azul":
+            echo '<body style="background-color:blue">';
+
+        break;
+        case "verde":
+            echo '<body style="background-color:green">';
+
+        break;
+        case "ninguno":
+            echo '<body style="background-color:white">';
+
+        break;
+    }
+
+
+}else{
+    echo "cookie no creada";
+}
+
+?>
